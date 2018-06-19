a68412149094bc96ed2f671fd2ae02eed72dbc69
Rosie Botelho <rosie.inthelight@gmail.com>
2018-06-18 22:33:44 -0300

    Atualizar exercícios

---
+++ javascript7.js
@@ -0,0 +1,15 @@
+let Meninos = ["João, Osvaldo, Igor, James, Neymar"];
+
+let Meninas = ["Ivete, Leila, Luiza, Ana, Lucia"];
+
+
+
+for(let Menino of Meninos){
+
+for(let Menina of Meninas){
+
+console.log(menina, "e", menino);
+
+}
+
+}
